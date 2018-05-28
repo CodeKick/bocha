2ffa335a5d3f395e0651a6dd27cc00d6693de928
davidcodekick <david.jensen@codekick.com>
2018-05-28 11:17:17 +0200

    sinon match stöd för CalledXWith

--- test/node/assert.tests.js
+++ test/node/assert.tests.js
@@ -8,60 +8,75 @@ module.exports = testCase('assert', {
         assert.equals(NaN, NaN);
     },
     'null !== hello': function () {
-        let error;
-        try {
-            assert.match(null, 'Hello');
-        }
-        catch (err) {
-            error = err;
-        }
-
+        let error = catchError(() => assert.match(null, 'Hello'));
         assert(error);
     },
-    'when assert.calledWith and stub was never call should give message saying it was never called': function () {
+    'when calledWith and stub was never call should give message saying it was never called': function () {
         let func = sinon.stub();
 
-        let error;
-        try {
-            assert.calledWith(func, {
-                foo: 'bar'
-            });
-        }
-        catch (err) {
-            error = err;
-        }
+        let error = catchError(() => {
+                assert.calledWith(func, {
+                    foo: 'bar'
+                });
+            }
+        );
 
         assert(error);
         assert.equals(error.message, 'expected stub to have been called at least once but was never called');
     },
-    'assert.startsWith': {
+    'calledTwiceWith:': {
+        'can use calledTwiceWith with matcher'() {
+            let stub = sinon.stub();
+            stub({
+                foo: 'bar'
+            });
+            stub({
+                bar: 'foo',
+                foo: 'bar'
+            });
+
+            assert.calledTwiceWith(stub, sinon.match({
+                foo: 'bar'
+            }));
+        },
+        'can use calledTwiceWith without matcher'() {
+            let stub = sinon.stub();
+            stub({
+                foo: 'bar'
+            });
+            stub({
+                bar: 'foo',
+                foo: 'bar'
+            });
+
+            let error = catchError(() => assert.calledTwiceWith(stub, { foo: '' }));
+            assert(error);
+        },
+    },
+    'startsWith:': {
         'actual is "abc" and expected is "ab" should pass': function () {
             assert.startsWith('abc', 'ab');
         },
         'actual is "cba" and expected is "ab" should fail': function () {
-            let error;
-            try {
-                assert.startsWith('cba', 'ab');
-            }
-            catch (err) {
-                error = err;
-            }
-
+            let error = catchError(() => assert.startsWith('cba', 'ab'));
             assert(error);
         },
         'actual is "[1, 2, 3]" and expected is "[1, 2]" should pass': function () {
             assert.startsWith([1, 2, 3], [1, 2]);
         },
         'actual is "[3, 2, 1]" and expected is "[1, 2]" should fail': function () {
-            let error;
-            try {
-                assert.startsWith([3, 2, 1], [1, 2]);
-            }
-            catch (err) {
-                error = err;
-            }
-
+            let error = catchError(() => assert.startsWith([3, 2, 1], [1, 2]));
             assert(error);
         }
-    }
+    },
+
 });
+
+function catchError(fn) {
+    try {
+        fn();
+    }
+    catch (err) {
+        return err;
+    }
+}
