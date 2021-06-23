98b9b6d206e754f5bdba07164974dc97da08bb92
arnesten <calle.arnesten@codekick.com>
2021-06-23 15:37:28 +0200

    Added es-module file for browser tests

---
+++ browser-entry.mjs
@@ -0,0 +1,18 @@
+import bocha from './browser-entry.js';
+
+export const testCase = bocha.testCase;
+export const assert = bocha.assert;
+export const refute = bocha.refute;
+export const sinon = bocha.sinon;
+export const stub = bocha.stub;
+export const spy = bocha.spy;
+export const fakeClock = bocha.fakeClock;
+export const waitStub = bocha.waitStub;
+export const timeout = bocha.timeout;
+export const dom = bocha.dom;
+export const defaults = bocha.defaults;
+export const defaultsDeep = bocha.defaultsDeep;
+export const localToUtc = bocha.localToUtc;
+export const utcToLocal = bocha.utcToLocal;
+export const click = bocha.dom.clickAndTick;
+export const setValue = bocha.dom.setValueAndTick;
